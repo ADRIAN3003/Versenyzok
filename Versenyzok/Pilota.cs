e10b405cbe9c39af963d2f7eca085157dde1ceb4
ADRIAN30 <medveadrian.szgya@gmail.com>
2022-10-25 12:51:03 +0200

    Add project files.

---
+++ Versenyzok/Pilota.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Versenyzok
+{
+    class Pilota
+    {
+        private string nev;
+
+        public string Nev
+        {
+            get { return nev; }
+            set { nev = value; }
+        }
+
+        private DateTime szulDatum;
+
+        public DateTime SzulDatum
+        {
+            get { return szulDatum; }
+            set { szulDatum = value; }
+        }
+
+        private string nemzetiseg;
+
+        public string Nemzetiseg
+        {
+            get { return nemzetiseg; }
+            set { nemzetiseg = value; }
+        }
+
+        private int rajtszam;
+
+        public int Rajtszam
+        {
+            get { return rajtszam; }
+            set { rajtszam = value; }
+        }
+
+        public Pilota(string nev, DateTime szulDatum, string nemzetiseg)
+        {
+            Nev = nev;
+            SzulDatum = szulDatum;
+            Nemzetiseg = nemzetiseg;
+        }
+
+        public Pilota(string nev, DateTime szulDatum, string nemzetiseg, int rajtszam) : this(nev, szulDatum, nemzetiseg)
+        {
+            Rajtszam = rajtszam;
+        }
+    }
+}
---
+++ Versenyzok/Program.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Versenyzok
+{
+    class Program
+    {
+        static List<Pilota> pilotak = new List<Pilota>();
+        static void Main(string[] args)
+        {
+            MasodikFeladat();
+            HarmadikFeladat();
+            NegyedikFeladat();
+            OtodikFeladat();
+            HatodikFeladat();
+            HetedikFeladat();
+
+            Console.ReadKey();
+        }
+
+        private static void HetedikFeladat()
+        {
+            Console.Write("7. feladat: ");
+
+            Dictionary<int, int> tmp = new Dictionary<int, int>();
+            string sor = "";
+            foreach (var pilota in pilotak)
+            {
+                if (pilota.Rajtszam != 0)
+                {
+                    if (tmp.ContainsKey(pilota.Rajtszam))
+                    {
+                        sor += pilota.Rajtszam + ", ";
+                    }
+                    else
+                    {
+                        tmp.Add(pilota.Rajtszam, 1);
+                    }
+                }
+            }
+            Console.WriteLine(sor.Substring(0, sor.Length - 2));
+        }
+
+        private static void HatodikFeladat()
+        {
+            //Console.WriteLine("6. feladat: " + pilotak.First(o => o.Rajtszam == pilotak.Where(i => i.Rajtszam != 0).Min(i => i.Rajtszam)).Nemzetiseg);
+            Console.WriteLine("6 feladat: " + pilotak.Where(i => i.Rajtszam != 0).OrderBy(o => o.Rajtszam).First().Nemzetiseg);
+        }
+
+        private static void OtodikFeladat()
+        {
+            Console.WriteLine("5. feladat:");
+            foreach (var pilota in pilotak)
+            {
+                if (pilota.SzulDatum < Convert.ToDateTime("1901. 01. 01."))
+                {
+                    Console.WriteLine($"\t{pilota.Nev} ({pilota.SzulDatum.ToShortDateString()})");
+                }
+            }
+        }
+
+        private static void NegyedikFeladat()
+        {
+            Console.WriteLine("4. feladat: " + pilotak.Last().Nev);
+        }
+
+        private static void HarmadikFeladat()
+        {
+            Console.WriteLine("3. feladat: " + pilotak.Count);
+        }
+
+        private static void MasodikFeladat()
+        {
+            using (StreamReader sr = new StreamReader("Adatfajl\\pilotak.csv"))
+            {
+                sr.ReadLine();
+                while (!sr.EndOfStream)
+                {
+                    string[] adat = sr.ReadLine().Split(';');
+                    int szam;
+                    if (!int.TryParse(adat[3], out szam))
+                    {
+                        szam = 0;
+                    }
+                    pilotak.Add(new Pilota(adat[0], Convert.ToDateTime(adat[1]), adat[2], szam));
+                    //if (adat[3] == String.Empty)
+                    //{
+                    //    pilotak.Add(new Pilota(adat[0], Convert.ToDateTime(adat[1]), adat[2]));
+                    //}
+                    //else
+                    //{
+                    //    pilotak.Add(new Pilota(adat[0], Convert.ToDateTime(adat[1]), adat[2], Convert.ToInt32(adat[3])));
+                    //}
+                }
+            }
+        }
+    }
+}
